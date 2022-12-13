fe826d5f5812845149154fcc454ebd89480c8495
Anthony Viramontes <amviramontes34@yahoo.com>
2022-12-12 23:49:43 -0800

    added row and column method to check for a specific number

--- src/main/java/soduku/SodukuSolver.java
+++ src/main/java/soduku/SodukuSolver.java
@@ -1,11 +1,42 @@
 package soduku;
 
 public class SodukuSolver {
+	
+	private static final int GRID_SIZE = 9;
+	
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		
-
+		int[][] board = {
+				{7, 0, 2, 0, 5, 0, 6, 0, 0},
+				{0, 0, 0, 0, 0, 3, 0, 0, 0},
+				{1, 0, 0, 0, 0, 9, 5, 0, 0},
+				{8, 0, 0, 0, 0, 0, 0, 9, 0},
+				{0, 9, 0, 0, 0, 0, 0, 0, 8},
+				{0, 0, 9, 7, 0, 0, 0, 0, 5},
+				{0, 0, 0, 2, 0, 0, 0, 0, 0},
+				{0, 0, 7, 0, 4, 0, 2, 0, 3}
+		};
+	}
+	
+	//check if number appears in row
+	public static boolean isNumberInRow(int[][] board, int number, int row) {
+		for (int i = 0; i < GRID_SIZE; i++) {
+			if (number == board[row][i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//check if number appears in row
+	public static boolean isNumberInColumn(int[][] board, int number, int column) {
+		for (int i = 0; i < GRID_SIZE; i++) {
+			if (number == board[i][column]) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
